a4d04898462ebab29ea35f61eceb1b3d5edcaef5
Battlecats59 <awelch6206@gmail.com>
2026-01-04 16:01:25 -0500

    v0.5.4

--- __init__.py
+++ __init__.py
@@ -42,9 +42,9 @@ from .rando.MiscRando import shuffle_batreaux_counts
 from .logic.LogicParser import parse_expression
 from .logic.Logic import ALL_REQUIREMENTS
 
-AP_VERSION = [0, 6, 2]
-WORLD_VERSION = [0, 5, 3]
-RANDO_VERSION = [0, 5, 3]
+AP_VERSION = [0, 6, 5]
+WORLD_VERSION = [0, 5, 4]
+RANDO_VERSION = [0, 5, 4]
 
 
 def run_client() -> None:
@@ -87,7 +87,7 @@ class SSWeb(WebWorld):
     rich_text_options_doc = True
 
 
-class SSContainer(APPlayerContainer, metaclass=AutoPatchRegister):
+class SSContainer(APPlayerContainer):
     """
     This class defines the container file for Skyward Sword.
     """
@@ -725,4 +725,5 @@ class SSWorld(World):
         }
 
         return slot_data
+
 
